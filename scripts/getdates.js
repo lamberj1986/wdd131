2375bf76e820120c4dab263bc56c6eb0a262f268
lamberj1986 <lamberj1986@gmail.com>
2024-06-28 16:57:19 -0400

    Updates for week 2 changes. Figured out JS issues.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,38 @@
-const currentyear = document.querySelector("#year");
+document.addEventListener('DOMContentLoaded', function() {
+    class DateManager {
+        lastModified;
+        copyYear;
 
-let lastModified = new Date(document.lastModified);
+        constructor(){
+            this.genCopyYear();
+            this.genLastModified();
+        }
+
+        genCopyYear(){
+            this.copyYear = new Date().getFullYear();
+        }
+
+        genLastModified(){
+            this.lastModified = document.lastModified;
+        }
+
+        displayLastModified(){
+            return this.lastModified.toString();
+        }
+
+        displayCopyYear(){
+            return `${this.copyYear}`;
+        }
+    }
+
+    function main(){
+        const manager = new DateManager();
+        const currentYear = document.getElementById('currentYear');
+        const lastModified = document.getElementById('lastModified');
+
+        currentYear.innerHTML = manager.displayCopyYear();
+        lastModified.innerHTML = `Last modified: ${manager.displayLastModified()}`;
+    }
+
+    main();
+});
--- scripts/temples.js
+++ scripts/temples.js
@@ -1,69 +1,71 @@
-class DateManager {
-    lastModified;
-    copyYear;
+document.addEventListener('DOMContentLoaded', function() {
+    class DateManager {
+        lastModified;
+        copyYear;
 
-    constructor(){
-        this.genCopyYear();
-        this.genLastModified();
+        constructor() {
+            this.genCopyYear();
+            this.genLastModified();
+        }
+
+        genCopyYear() {
+            this.copyYear = new Date().getFullYear();
+        }
+
+        genLastModified() {
+            this.lastModified = document.lastModified;
+        }
+
+        displayLastModified() {
+            return this.lastModified.toString();
+        }
+
+        displayCopyYear() {
+            return `${this.copyYear}`;
+        }
     }
 
-    genCopyYear(){
-        this.copyYear = new Date().getFullYear();
+    class HamburgerMenu {
+        btn;
+        nav;
+
+        constructor(buttonEl, navEl) {
+            this.btn = buttonEl;
+            this.nav = navEl
+        }
+
+        setMenuListener() {
+            // The .open class must be defined in the stylesheet.
+            this.btn.addEventListener('click', () => {
+                this.nav.classList.toggle('open');
+                this.btn.classList.toggle('open');
+            });
+        }
     }
 
-    genLastModified(){
-        this.lastModified = document.lastModified;
+    function pageTitle() {
+        const pageTitleEl = document.getElementById('page-title');
+        const activePageEl = document.querySelector('.active');
+
+        pageTitleEl.textContent = activePageEl.textContent;
     }
 
-    displayLastModified(){
-        return this.lastModified.toString();
+    function main() {
+        const manager = new DateManager();
+
+        const menuBtn = document.querySelector('.menu-button');
+        const navi = document.querySelector('.site-nav');
+        const hamburger = new HamburgerMenu(menuBtn, navi);
+        hamburger.setMenuListener();
+
+        const currentYear = document.getElementById('currentYear');
+        const lastModified = document.getElementById('lastModified');
+
+        currentYear.innerHTML = manager.displayCopyYear();
+        lastModified.innerHTML = `Last modified: ${manager.displayLastModified()}`;
+
+        pageTitle();
     }
 
-    displayCopyYear(){
-        return `${this.copyYear}`;
-    }
-}
-
-class HamburgerMenu {
-    btn;
-    nav;
-
-    constructor(buttonEl, navEl){
-        this.btn = buttonEl;
-        this.nav = navEl
-    }
-
-    setMenuListener(){
-        //the .open class must be defined in the stylesheet.
-        this.btn.addEventListener('click', () => {
-            this.nav.classList.toggle('open');
-            this.btn.classList.toggle('open');
-        })
-    }
-}
-
-function pageTitle(){
-    const pageTitleEl = document.getElementById('page-title');
-    const activePageEl = document.querySelector('.active');
-
-    pageTitleEl.textContent = activePageEl.textContent;
-}
-
-function main() {
-    const manager = new DateManager();
-
-    const menuBtn = document.querySelector('.menu-btn');
-    const navi = document.querySelector('.site-nav');
-    const hamburger = new HamburgerMenu(menuBtn, navi);
-    hamburger.setMenuListener();
-
-    const currentYear = document.getElementById('currentYear');
-    const lastModified = document.getElementById('lastModified');
-
-    currentYear.innerHTML = manager.displayCopyYear();
-    lastModified.innerHTML = `Last modified: ${manager.displayLastModified()}`;
-
-    pageTitle();
-}
-
-main();
+    main();
+});
